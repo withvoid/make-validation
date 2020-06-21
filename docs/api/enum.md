73406a215f43217bae336981183d5c1ceaaf3d5b
Adeel Imran <adeelimranr@gmail.com>
2020-06-21 23:59:22 +0200

    docusaurus website (#1)
    * update readme
    
    * Added content for website

---
+++ docs/api/enum.md
@@ -0,0 +1,59 @@
+---
+id: enum
+title: Enum
+sidebar_label: Enum API
+---
+
+Enum validation can be done with the following options
+
+## Options
+
+- `options.enum` (default `{}`, required: `yes`) It can be of 2 types string and
+    object.
+
+```js
+checks: {
+  userType1: { type: types.enum, options: { enum: 'admin user' } },
+  userType2: {
+    type: types.enum,
+    options: {
+      enum: { 0: 'admin', 1: 'user' },
+    },
+  },
+}
+```
+
+- If `options.enum` a `string` the enum is seperated by space.
+- If `options.enum` an `object` the enum are the values in the objects.
+
+## Example
+
+```js
+const result = makeValidation((types) => {
+  return {
+    payload: {
+      type1: 'eagle', // invalid
+      type2: 'admin', // valid
+      type3: 'elephant', // invalid
+      type4: 'user', // valid
+    },
+    checks: {
+      type1: { type: types.enum, options: { enum: 'admin user' } },
+      type2: { type: types.enum, options: { enum: 'admin user' } },
+      type3: {
+        type: types.enum,
+        options: {
+          enum: { 0: 'admin', 1: 'user' },
+        },
+      },
+      type4: {
+        type: types.enum,
+        options: {
+          enum: { 0: 'admin', 1: 'user' },
+        },
+      },
+    },
+  };
+});
+```
+
---
+++ website/pages/en/index.js
@@ -0,0 +1,153 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const React = require('react');
+
+const CompLibrary = require('../../core/CompLibrary.js');
+
+const MarkdownBlock = CompLibrary.MarkdownBlock; /* Used to read markdown */
+const Container = CompLibrary.Container;
+const GridBlock = CompLibrary.GridBlock;
+
+class HomeSplash extends React.Component {
+  render() {
+    const {siteConfig, language = ''} = this.props;
+    const {baseUrl, docsUrl} = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+    const SplashContainer = props => (
+      <div className="homeContainer">
+        <div className="homeSplashFade">
+          <div className="wrapper homeWrapper">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const ProjectTitle = props => (
+      <h2 className="projectTitle">
+        {props.title}
+        <small>{props.tagline}</small>
+      </h2>
+    );
+
+    const PromoSection = props => (
+      <div className="section promoSection">
+        <div className="promoRow">
+          <div className="pluginRowBlock">{props.children}</div>
+        </div>
+      </div>
+    );
+
+    const Button = props => (
+      <div className="pluginWrapper buttonWrapper">
+        <a className="button" href={props.href} target={props.target}>
+          {props.children}
+        </a>
+      </div>
+    );
+
+    return (
+      <SplashContainer>
+        <div className="inner">
+          <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
+          <PromoSection>
+            <Button href={docUrl('getting-started/install')}>Install</Button>
+            <Button href={docUrl('examples/express-example')}>Example ExpressJS</Button>
+            <Button
+              href="https://npm.runkit.com/%40withvoid%2Fmake-validation"
+              target="_blank"
+            >
+              Try It Out
+            </Button>
+          </PromoSection>
+        </div>
+      </SplashContainer>
+    );
+  }
+}
+
+class Index extends React.Component {
+  render() {
+    const {config: siteConfig, language = ''} = this.props;
+    const {baseUrl} = siteConfig;
+
+    const Block = props => (
+      <Container
+        padding={['bottom', 'top']}
+        id={props.id}
+        background={props.background}>
+        <GridBlock
+          align="center"
+          contents={props.children}
+          layout={props.layout}
+        />
+      </Container>
+    );
+
+    const Features = () => (
+      <Block layout="fourColumn">
+        {[
+          {
+            content: 'Easily validate an API request withing 2 minutes.',
+            image: `${baseUrl}img/undraw_code_review.svg`,
+            imageAlign: 'top',
+            title: 'Validate quickly',
+          },
+          {
+            content: 'Consistent API response for all error responses',
+            image: `${baseUrl}img/undraw_online.svg`,
+            imageAlign: 'top',
+            title: 'Standard API response',
+          },
+        ]}
+      </Block>
+    );
+
+    const Showcase = () => {
+      if ((siteConfig.users || []).length === 0) {
+        return null;
+      }
+
+      const showcase = siteConfig.users
+        .filter(user => user.pinned)
+        .map(user => (
+          <a href={user.infoLink} key={user.infoLink}>
+            <img src={user.image} alt={user.caption} title={user.caption} />
+          </a>
+        ));
+
+      const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
+
+      return (
+        <div className="productShowcaseSection paddingBottom">
+          <h2>Who is Using This?</h2>
+          <p>This project is used by all these people</p>
+          <div className="logos">{showcase}</div>
+          <div className="more-users">
+            <a className="button" href={pageUrl('users.html')}>
+              More {siteConfig.title} Users
+            </a>
+          </div>
+        </div>
+      );
+    };
+
+    return (
+      <div>
+        <HomeSplash siteConfig={siteConfig} language={language} />
+        <div className="mainContainer">
+          <Features />
+          <Showcase />
+        </div>
+      </div>
+    );
+  }
+}
+
+module.exports = Index;
